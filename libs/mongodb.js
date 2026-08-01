8b7dfcdb146ad9507ba18d83df154971fbf6707f
mahbuba-bithi <mahbubabithi2393@gmail.com>
2026-07-31 20:06:52 -0700

    backend and frontend done

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = (){
+const connectMongoDB = async ()=>{
     try{
-        mongoose.connect(process.env.MONGO_DB_URI)
+       await mongoose.connect(process.env.MONGO_DB_URI)
         console.log("connected to mongodb")
     } catch(error){
        console.log(error)
